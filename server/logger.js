f57403f82596454c903045cfbcc50bdd34609527
Vinay Joshi <vinay01joshi@gmail.com>
2020-05-10 22:08:05 +0530

    adding logs as stream

--- server/logger.js
+++ server/logger.js
@@ -12,12 +12,12 @@ let streams = null;
 if (process.env.NODE_ENV === 'production' && process.env.MACHINE !== 'heroku') {
     streams = [{ path: 'logs/' + loggerName + '.log' }];
 }
-///else if (process.env.NODE_ENV === 'production' && process.env.MACHINE === 'heroku') {
-//     streams = [
-//         { path: 'logs/' + loggerName + '.log' },
-//         { stream: process.stdout },
-//     ];
-// } 
+else if (process.env.NODE_ENV === 'production' && process.env.MACHINE === 'heroku') {
+    streams = [
+        { path: 'logs/' + loggerName + '.log' },
+        { stream: process.stdout },
+    ];
+}
 else {
     streams = [
         { stream: process.stdout }
