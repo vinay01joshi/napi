8b675033b19f53187c709dcebbcd695a6114e3de
Vinay Joshi <vinay01joshi@gmail.com>
2020-05-10 22:11:09 +0530

    Modifying logger.js

--- server/logger.js
+++ server/logger.js
@@ -8,15 +8,14 @@ const loggerName = `logs-${logStartDate}`;
 
 
 let streams = null;
-
-if (process.env.NODE_ENV === 'production' && process.env.MACHINE !== 'heroku') {
-    streams = [{ path: 'logs/' + loggerName + '.log' }];
-}
-else if (process.env.NODE_ENV === 'production' && process.env.MACHINE === 'heroku') {
+if (process.env.NODE_ENV === 'production' && process.env.MACHINE === 'heroku') {
     streams = [
         { path: 'logs/' + loggerName + '.log' },
         { stream: process.stdout },
     ];
+}
+else if (process.env.NODE_ENV === 'production') {
+    streams = [{ path: 'logs/' + loggerName + '.log' }];
 }
 else {
     streams = [
